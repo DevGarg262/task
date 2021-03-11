5141ce6310b03864809cb24fee0ab614d080e831
Dev Garg <dgarg5908@gmail.com>
2021-03-11 23:14:47 +0530

    Update and rename modal.jsx to Signupmodal.jsx

--- src/components/Signupmodal.jsx
+++ src/components/Signupmodal.jsx
@@ -1,4 +1,4 @@
-import './modal.css';
+import './Signupmodal.css';
 
 const Modal = ({ handleClose, show, children }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
