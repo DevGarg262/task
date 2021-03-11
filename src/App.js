56c0c8abcbc5aa30ef5ea279406299efbf1d3c47
Dev Garg <dgarg5908@gmail.com>
2021-03-11 22:46:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from "react-router-dom";
-
 import About from "./components/about"
-import Signup from "./components/signup"
-// import Setting from "./components/Settings";
-// import UserView from "./components/UserView";
-
-// jsx => js
-// import MyVerticallyCenteredModal from './components/signup'
-// import Smodal from "./components/dashboard"
-
-
-// render(<App />);
-
-
-
 
 function App() {
-
-
   return (
-
-
-
     <React.Fragment>
       <Router>
           <Header />
